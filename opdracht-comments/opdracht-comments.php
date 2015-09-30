ebed47cd4c3461d1d857e0b1d78b16242fff95cb
rubends <rubendeswaef@gmail.com>
2015-09-30 10:08:58 +0200

    Oplossing toegevoegd

---
+++ opdracht-comments/opdracht-comments.php
@@ -0,0 +1,23 @@
+<?php
+        $naam = 'Ruben';
+        $achternaam = 'De Swaef';
+
+/*
+		Ruben De Swaef
+		rubendeswaef@gmail.com
+*/
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>hallo, ik heet:</h1>
+        <?php echo $naam ?>
+        <?php echo $achternaam ?>
+        
+        
+    </body>
+</html>
