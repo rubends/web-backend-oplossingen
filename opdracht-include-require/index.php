675de1043de2a09930eeb29e28aacf351356feaa
rubends <rubendeswaef@gmail.com>
2015-10-21 09:49:10 +0200

    oplossing toegevoegd

---
+++ opdracht-include-require/index.php
@@ -0,0 +1,13 @@
+<?php
+    $artikels = array(
+					array('title'=>'Artikel1', 'body' => 'Dit is de body van artikel1', 'tags' 	=> 'kernwoorden artikel 1',), 
+                    array('title'=>'Artikel2', 'body' => 'Dit is de body van artikel2', 'tags' 	=> 'kernwoorden artikel 2',),
+                    array('title'=>'Artikel3', 'body' => 'Dit is de body van artikel3', 'tags' 	=> 'kernwoorden artikel 3',));
+					
+    
+    include 'header-partial.html';
+    include 'body-partial.html';
+    include 'footer-partial.html';
+
+
+?>
