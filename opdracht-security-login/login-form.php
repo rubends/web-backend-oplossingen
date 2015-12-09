ceeaf40c1529f0dff08cbef7efc62d0b498ae014
rubends <rubendeswaef@gmail.com>
2015-12-09 19:28:55 +0100

    fixed security

--- opdracht-security-login/login-form.php
+++ opdracht-security-login/login-form.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
+	session_start();
+	if (isset($_COOKIE[login])) {
+		header("Location: dashboard.php?");
+	}
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- opdracht-security-login/registratie-form.php
+++ opdracht-security-login/registratie-form.php
@@ -1,6 +1,8 @@
 <?php
 	session_start();
-
+	if (isset($_COOKIE[login])) {
+		header("Location: dashboard.php?");
+	}
 ?>
 
 <!DOCTYPE html>
